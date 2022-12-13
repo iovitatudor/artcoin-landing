ac660386db4d8bf7a9d3428cbdcf73b24de0cf34
Tudor Iovita <iovitatudor21@gmail.com>
2022-12-13 13:40:38 +0200

    feat(near-connection):add smart-contract

---
+++ contract/target/wasm32-unknown-unknown/release/fungible_token.d
@@ -0,0 +1,1 @@
+/Users/tudoriovita/Documents/actual/artcoin-front/contract/target/wasm32-unknown-unknown/release/fungible_token.wasm: /Users/tudoriovita/Documents/actual/artcoin-front/contract/src/lib.rs
